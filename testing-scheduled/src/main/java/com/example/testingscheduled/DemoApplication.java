99e82f59642b2d52a9f83aed486a76d54bcbdb83
Dariusz Mydlarz <dmydlarz@gmail.com>
2020-12-07 21:26:44 +0100

    Drop unused code

--- testing-scheduled/src/main/java/com/example/testingscheduled/DemoApplication.java
+++ testing-scheduled/src/main/java/com/example/testingscheduled/DemoApplication.java
@@ -2,21 +2,14 @@ package com.example.testingscheduled;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.util.concurrent.CountDownLatch;
 
 @EnableScheduling
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-
-		final CountDownLatch closeLatch = new CountDownLatch(1);
-		Runtime.getRuntime().addShutdownHook(new Thread(closeLatch::countDown));
-		closeLatch.await();
 	}
 
 }
