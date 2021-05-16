62b99990f12b54254706eed8d7f958387f5013ca
Dariusz Mydlarz <dmydlarz@gmail.com>
2021-05-16 12:35:52 +0200

    Init spring-security-token project

---
+++ spring-security-token/src/main/java/pl/sztukakodu/springsecuritytoken/SpringSecurityTokenApplication.java
@@ -0,0 +1,13 @@
+package pl.sztukakodu.springsecuritytoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityTokenApplication.class, args);
+    }
+
+}
---
+++ spring-security-token/src/test/java/pl/sztukakodu/springsecuritytoken/SpringSecurityTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sztukakodu.springsecuritytoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTokenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
